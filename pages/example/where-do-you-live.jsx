8e2c523ea06204d7d4266c2d4f7fe1f4d555526b
Craig Taylor <craig.taylor3@engineering.digital.dwp.gov.uk>
2023-05-16 11:15:08 +0100

    where do you live

---
+++ pages/example/where-do-you-live.jsx
@@ -0,0 +1,52 @@
+import { Label, Legend, Radios, RadioItem } from 'govuk-ui';
+import Head from 'next/head';
+import { getPageData } from '@/helpers/get-page-data';
+import { TwoThirdsLayout } from '@/layouts/TwoThirdsLayout';
+import { urls } from '@/lib/urls';
+
+export const getServerSideProps = async (context) => await getPageData(context);
+
+export default function WhereDoYouLive({ pageId, data, errors }) {
+  console.log(data);
+  return (
+    <TwoThirdsLayout pageId={pageId} data={data} errors={errors}>
+      <Head>
+        <title>Where do you live</title>
+      </Head>
+      
+      <Radios name="whereDoYouLive" value={ data?.whereDoYouLive}>
+        <Legend
+          classes="govuk-fieldset__legend--l"
+          isPageHeading
+        >
+          Where do you live?
+        </Legend>
+        <RadioItem value="england">
+          <Label>
+            England
+          </Label>
+        </RadioItem>
+        <RadioItem value="scotland">
+          <Label>
+            Scotland
+          </Label>
+        </RadioItem>
+        <RadioItem value="wales">
+          <Label>
+            Wales
+          </Label>
+        </RadioItem>
+        <RadioItem value="northernIreland">
+          <Label>
+            Northern Ireland
+          </Label>
+        </RadioItem>
+        <RadioItem value="somwewhereElse">
+          <Label>
+            Somewhere Else
+          </Label>
+        </RadioItem>
+      </Radios>
+    </TwoThirdsLayout>
+  )
+}
