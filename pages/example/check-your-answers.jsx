9fe0ade5a3bf6487e22a3dc90c1f398d9c345c0c
nathan.maybrey <nathan.maybrey@engineering.digital.dwp.gov.uk>
2023-05-17 11:38:55 +0100

    Fix typos and CYA data guard

--- pages/example/check-your-answers.jsx
+++ pages/example/check-your-answers.jsx
@@ -18,7 +18,7 @@ import Link from 'next/link';
 export const getServerSideProps = async (context) => {
   const cookies = context.req.cookies;
   const sessionId = cookies[process.env.SESSION_COOKIE_NAME];
-  const data = await getFormData(sessionId);
+  const data = await getFormData(sessionId) || null;
   return {
     props: {
       data
@@ -34,11 +34,9 @@ const capitalise = (answer) => {
 }
 
 export default function CheckYourAnswers({ data }) {
-  const { 'dateOfBirth-day': day, 'dateOfBirth-month': month, 'dateOfBirth-year': year } = data[urls.dateOfBirth];
+  // Format date of birth into a readable format
+  const { 'dateOfBirth-day': day, 'dateOfBirth-month': month, 'dateOfBirth-year': year } = data?.[urls.dateOfBirth] || {};
   const dateOfBirth = DateTime.fromFormat(`${day}-${month}-${year}`, 'd-m-yyyy').toFormat('d MMMM yyyy');
-  const contactPreferences = data[urls.contactPreferences].contactPreferences;
-  const email = data[urls.contactPreferences].emailAddress;
-  const phoneNumber = data[urls.contactPreferences].phoneNumber;
 
   return (
     <TwoThirdsLayout hideContinueButton>
@@ -50,77 +48,79 @@ export default function CheckYourAnswers({ data }) {
         Check your answers before sending your application
       </Typography>
 
-      <SummaryList id="your-details">
-        <SummaryListItem>
-          <SummaryListKey>Full name</SummaryListKey>
-          <SummaryListValue>
-            { `${data[urls.fullName].firstName} ${data[urls.fullName].middleNames} ${data[urls.fullName].lastName}` }
-          </SummaryListValue>
-          <SummaryListActions>
-            <a href={`${urls.fullName}#fullName`} className="govuk-link govuk-link--no-visited-state">Change</a>
-          </SummaryListActions>
-        </SummaryListItem>
-
-        <SummaryListItem>
-          <SummaryListKey>Date of birth</SummaryListKey>
-          <SummaryListValue>{ dateOfBirth }</SummaryListValue>
-          <SummaryListActions>
-            <a href={`${urls.dateOfBirth}#dateOfBirth-day`} className="govuk-link govuk-link--no-visited-state">Change</a>
-          </SummaryListActions>
-        </SummaryListItem>
-
-        <SummaryListItem>
-          <SummaryListKey>Where you live</SummaryListKey>
-          <SummaryListValue>
-            <Typography>{ capitalise(data[urls.whereDoYouLive].whereDoYouLive) }</Typography></SummaryListValue>
-          <SummaryListActions>
-            <Link href={`${urls.whereDoYouLive}#whereDoYouLive`} className="govuk-link govuk-link--no-visited-state">Change</Link>
-          </SummaryListActions>
-        </SummaryListItem>
-
-        { data[urls.whereDoYouLiveOther] && (
+      { data && (
+        <SummaryList id="your-details">
           <SummaryListItem>
-            <SummaryListKey>Where you live other</SummaryListKey>
+            <SummaryListKey>Full name</SummaryListKey>
             <SummaryListValue>
-              <Typography>{ capitalise(data[urls.whereDoYouLiveOther].whereDoYouLiveOther) }</Typography></SummaryListValue>
+              { `${data[urls.fullName].firstName} ${data[urls.fullName].middleNames} ${data[urls.fullName].lastName}` }
+            </SummaryListValue>
             <SummaryListActions>
-              <Link href={`${urls.whereDoYouLiveOther}#whereDoYouLiveOther`} className="govuk-link govuk-link--no-visited-state">Change</Link>
+              <a href={`${urls.fullName}#fullName`} className="govuk-link govuk-link--no-visited-state">Change</a>
             </SummaryListActions>
           </SummaryListItem>
-        )}
 
-        <SummaryListItem>
-          <SummaryListKey>Contact preferences</SummaryListKey>
-          <SummaryListValue>
-            <ul className="govuk-list govuk-list--bullet">
-              { contactPreferences.map((preference) => <li key={preference}>{ capitalise(preference) }</li>) }
-            </ul>
-          </SummaryListValue>
-          <SummaryListActions>
-            <a href={`${urls.contactPreferences}#contactPreferences`} className="govuk-link govuk-link--no-visited-state">Change</a>
-          </SummaryListActions>
-        </SummaryListItem>
-
-        { contactPreferences.includes('email') && (
           <SummaryListItem>
-            <SummaryListKey>Email address</SummaryListKey>
-            <SummaryListValue>{ email }</SummaryListValue>
+            <SummaryListKey>Date of birth</SummaryListKey>
+            <SummaryListValue>{ dateOfBirth }</SummaryListValue>
             <SummaryListActions>
-              <a href={`${urls.contactPreferences}#emailAddress`} className="govuk-link govuk-link--no-visited-state">Change</a>
+              <a href={`${urls.dateOfBirth}#dateOfBirth-day`} className="govuk-link govuk-link--no-visited-state">Change</a>
             </SummaryListActions>
           </SummaryListItem>
-        )}
 
-        { contactPreferences.includes('phone') && (
           <SummaryListItem>
-            <SummaryListKey>Telephone number</SummaryListKey>
-            <SummaryListValue>{ phoneNumber }</SummaryListValue>
+            <SummaryListKey>Where you live</SummaryListKey>
+            <SummaryListValue>
+              <Typography>{ capitalise(data[urls.whereDoYouLive].whereDoYouLive) }</Typography></SummaryListValue>
             <SummaryListActions>
-              <a href={`${urls.contactPreferences}#phoneNumber`} className="govuk-link govuk-link--no-visited-state">Change</a>
+              <Link href={`${urls.whereDoYouLive}#whereDoYouLive`} className="govuk-link govuk-link--no-visited-state">Change</Link>
             </SummaryListActions>
           </SummaryListItem>
-        )}
-      </SummaryList>
+
+          { data[urls.whereDoYouLiveOther] && (
+            <SummaryListItem>
+              <SummaryListKey>Where you live other</SummaryListKey>
+              <SummaryListValue>
+                <Typography>{ capitalise(data[urls.whereDoYouLiveOther].whereDoYouLiveOther) }</Typography></SummaryListValue>
+              <SummaryListActions>
+                <Link href={`${urls.whereDoYouLiveOther}#whereDoYouLiveOther`} className="govuk-link govuk-link--no-visited-state">Change</Link>
+              </SummaryListActions>
+            </SummaryListItem>
+          )}
+
+          <SummaryListItem>
+            <SummaryListKey>Contact preferences</SummaryListKey>
+            <SummaryListValue>
+              <ul className="govuk-list govuk-list--bullet">
+                { data?.[urls.contactPreferences].contactPreferences.map((preference) => <li key={preference}>{ capitalise(preference) }</li>) }
+              </ul>
+            </SummaryListValue>
+            <SummaryListActions>
+              <a href={`${urls.contactPreferences}#contactPreferences`} className="govuk-link govuk-link--no-visited-state">Change</a>
+            </SummaryListActions>
+          </SummaryListItem>
+
+          { data?.[urls.contactPreferences].contactPreferences.includes('email') && (
+            <SummaryListItem>
+              <SummaryListKey>Email address</SummaryListKey>
+              <SummaryListValue>{ data?.[urls.contactPreferences].emailAddress }</SummaryListValue>
+              <SummaryListActions>
+                <a href={`${urls.contactPreferences}#emailAddress`} className="govuk-link govuk-link--no-visited-state">Change</a>
+              </SummaryListActions>
+            </SummaryListItem>
+          )}
+
+          { data?.[urls.contactPreferences].contactPreferences.includes('phone') && (
+            <SummaryListItem>
+              <SummaryListKey>Telephone number</SummaryListKey>
+              <SummaryListValue>{ data?.[urls.contactPreferences].phoneNumber }</SummaryListValue>
+              <SummaryListActions>
+                <a href={`${urls.contactPreferences}#phoneNumber`} className="govuk-link govuk-link--no-visited-state">Change</a>
+              </SummaryListActions>
+            </SummaryListItem>
+          )}
+        </SummaryList>
+      )}
 
       <Typography variant="m" component="h2">
         Now send your application
