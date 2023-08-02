390f6c301832faa8671c48bb60d48a5329aa848b
nathan.maybrey <nathan.maybrey@engineering.digital.dwp.gov.uk>
2023-08-02 14:53:00 +0100

    chore(dependencies): Update out of date dependencies and default session storage to file

--- next.config.js
+++ next.config.js
@@ -1,29 +1,29 @@
-const { i18n } = require('./next-i18next.config');
+const { i18n } = require("./next-i18next.config");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone', // Needed for docker,
+  output: "standalone", // Needed for docker,
   env: {
-    SESSION_COOKIE_NAME: process.env.SESSION_COOKIE_NAME || 'session_cookie',
-    SESSION_STORAGE: process.env.SESSION_STORAGE || 'redis', // file or redis
-    REDIS_HOST: process.env.REDIS_HOST || 'localhost',
+    SESSION_COOKIE_NAME: process.env.SESSION_COOKIE_NAME || "session_cookie",
+    SESSION_STORAGE: process.env.SESSION_STORAGE || "file", // file or redis
+    REDIS_HOST: process.env.REDIS_HOST || "localhost",
     REDIS_PORT: process.env.REDIS_PORT || 6380,
   },
   i18n,
   async headers() {
     return [
       {
-        source: '/api/form-handler',
+        source: "/api/form-handler",
         headers: [
           {
-            key: 'Cache-Control',
-            value: 'no-store, max-age=0',
+            key: "Cache-Control",
+            value: "no-store, max-age=0",
           },
         ],
       },
     ];
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
